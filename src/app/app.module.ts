3bbc62fbb1a348d7a0ac011f77ac3cd98c359292
Baljeet Singh <bsinghs.007@gmail.com>
2019-09-19 11:06:41 +0530

    add ngrx store, store-devtools, effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([AppEffects]) ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
